a6e254601f8eb2adde9676f3b40ea254b2dff68c
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-02 01:53:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,30 +1,28 @@
 require 'pry'
-aoa = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
+src = [ 
+  [10, 11], 
+  [99, 50, 3, 4], 
+  [23, 41]
 ]
-src = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
-
-
 
 
 def find_even_values(src)
- count = 0
- ev = []
+ row = 0
   while count < src.length do
-    ic = 0
-    while ic < src[count].length do
-      tes = src[count][ic]  % 2
-        if tes == 0
-          ev.push(src[count][ic]) 
-          ic += 1
-        else
-        ic += 1
+    element = 0
+    while element < src[row].length do
+    puts src[row][element]
+    element += 1
     end
     counts += 1
   end
   ev
 end
+binding.pry
 
-
+tes = src[row][element]  % 2
+        if tes == 0
+          ev.push(src[count][ic]) 
+          ic += 1
+        else
+        ic += 1
