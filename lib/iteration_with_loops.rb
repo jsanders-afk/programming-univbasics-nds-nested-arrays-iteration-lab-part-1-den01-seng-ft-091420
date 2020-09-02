4c693b336f5cd55ab2a45fc92a403ef08ea7447b
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-02 02:00:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,8 +11,7 @@ def find_even_values(src)
   while row < src.length do
   element = 0
     while element < src[row].length do
-      tes = src[row][element].to_f
-      tes % 2 
+      tes = src[row][element].to_i % 2
         if tes == 0
             p "ping"
         else
